59761f2a82013819884e1ac53d2b37aad21a8ae9
Bronbronatron <bronwyn93@gmail.com>
2021-08-04 22:38:41 +0100

    making modifications to server

--- src/main/java/com/bron/grpc/GRPCServer.java
+++ src/main/java/com/bron/grpc/GRPCServer.java
@@ -1,14 +1,28 @@
 package com.bron.grpc;
 
 import java.io.IOException;
+import java.util.logging.Logger;
 
 import BankingApp.BankingAppService;
 import io.grpc.Server;
 import io.grpc.ServerBuilder;
 
 public class GRPCServer {
-
+	
+	
+	private static final Logger logger = Logger.getLogger(GRPCServer.class.getName());
+	
 	public static void main(String[] args) throws IOException, InterruptedException {
+		
+		GRPCServer budgetServer = new GRPCServer(); 
+		int port = 9097;
+		String service_type = "_grpc._tcp.local.";
+		String service_name = "GrpcServer";
+		JMDNS.SimpleServiceRegistration ssr = new JMDNS.SimpleServiceRegistration();
+		ssr.run(port, service_type, service_name);
+
+		
+	try {
 	Server server = ServerBuilder.forPort(9097).addService(new BankingAppService()).build(); 
 	
 	server.start();
@@ -17,6 +31,17 @@ public class GRPCServer {
 	
 	server.awaitTermination();
 	
+	} catch (IOException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+		
+	} catch (InterruptedException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
 	}
-
+    
+    logger.info("Server started, listening on " + port);
+    
+	}	    
+   
 }
