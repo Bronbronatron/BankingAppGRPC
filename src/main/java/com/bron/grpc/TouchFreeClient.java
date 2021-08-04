3e35739b86cd095039fdb03d93d9704a90049a14
Bronbronatron <bronwyn93@gmail.com>
2021-08-04 22:39:55 +0100

    modifying touch free client and server

--- src/main/java/com/bron/grpc/TouchFreeClient.java
+++ src/main/java/com/bron/grpc/TouchFreeClient.java
@@ -1,9 +1,41 @@
 package com.bron.grpc;
 
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+
 public class TouchFreeClient {
+
+	
 
 	public static void main(String[] args) {
 		
+	   TouchFreeClient touch = new TouchFreeClient();
+	   touch.run();
+
+	}
+		
+    private void run() {
+    	
+    	ManagedChannel TouchChannel = ManagedChannelBuilder.forAddress("LocalHost2", 9098).usePlaintext().build();		
+    	doTouchFreeUnaryCall(TouchChannel);
+			
+			
+			}
+    
+    
+    
+	private void doTouchFreeUnaryCall(ManagedChannel channel) {
+
+		touchFreeATMGrpc.touchFreeATMBlockingStub TouchFreeBudgetStub = touchFreeATMGrpc.newBlockingStub(channel);
+
+		inputPin pin = inputPin.newBuilder().setPinNumber(1111).build();
+
+		// call rpc and get back a response
+		authenticationMessage message = TouchFreeBudgetStub.authenticateCard(pin);
+		
+		// print the result
+		System.out.println(message.getWelcomeMessage());
+
 	}
 
 }
--- src/main/java/com/bron/grpc/TouchFreeGRPCServer.java
+++ src/main/java/com/bron/grpc/TouchFreeGRPCServer.java
@@ -1,19 +1,48 @@
 package com.bron.grpc;
 
 import java.io.IOException;
+import java.util.logging.Logger;
 
 import BankingApp.TouchFreeService;
+import JMDNS.SimpleServiceRegistration;
 import io.grpc.Server;
 import io.grpc.ServerBuilder;
 
 public class TouchFreeGRPCServer{
 	
+	private static final Logger logger = Logger.getLogger(TouchFreeGRPCServer.class.getName());
+	
+	
 	public static void main(String[] args) throws IOException {
+		
+		
+		int port = 9098;
+		String service_type_2 = "_grpc._tcp.local.2";
+		String service_name_2 = "GrpcServer2";
+		SimpleServiceRegistration ssr2 = new SimpleServiceRegistration();
+		ssr2.run(port, service_type_2, service_name_2);
+		
+		try {
 		
 		Server TouchFreeServer = ServerBuilder.forPort(9098).addService(new TouchFreeService()).build();
 		
 		TouchFreeServer.start();
 		System.out.println("Server started at " + TouchFreeServer.getPort());
+		
+		TouchFreeServer.awaitTermination();
+
+		
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	    
+	    logger.info("Server started, listening on " + port);
+	    
+		}	    
+	   
 	}
-	
-}
